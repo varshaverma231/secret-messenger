f85d97bf8b09b3c7403e479146480db2cdc1f815
Aakash Yadav <aakash.yadav@squareboat.com>
2019-06-05 10:21:04 +0530

    ADD: Incrementing characters

--- secret_messages/encrypt_message.js
+++ secret_messages/encrypt_message.js
@@ -18,3 +18,25 @@ for(var i = 0; i < 128; i++) {
 }
 
 
+// Incrementing a character
+
+// Convert Character to charcode > Increment > Convert Back to Character
+plain_text.charAt(0);
+
+var charCode = plain_text.charCodeAt(0);
+charCode = charCode + 1;
+
+console.log(String.fromCharCode(charCode));
+
+for(var i = 0; i < plain_text.length; i++ ){
+    var char_code = plain_text.charCodeAt(i);
+    char_code = char_code + 1;
+    
+    var new_char = String.fromCharCode(char_code);
+
+    plain_text = plain_text.substr(0, i) + new_char + plain_text.substr(i+1);
+}
+
+console.log(plain_text);
+
+
